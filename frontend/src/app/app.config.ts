256ae3794c42eb7b987ef2d07c77ae5050f48465
Vitor Queiroz <vitoralencar881@gmail.com>
2026-05-21 23:57:02 -0300

    add Firebase

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,12 +3,23 @@ import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 
+// ESTOU IMPORTANDO O FIREBASE
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+import { firebaseConfig } from '../conteudoFirebase/firebase.config';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withInMemoryScrolling({
       anchorScrolling: 'enabled', 
       scrollPositionRestoration: 'enabled'
-    }))
+    })
+  ),
+
+  //AQUI ESTA O FIREBASE
+  provideFirebaseApp(() => initializeApp(firebaseConfig)),
+  provideFirestore(() => getFirestore())  
   ]
 };
---
+++ frontend/src/conteudoFirebase/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAMu_hAj9j8MuG1yf7Uwwqhp_Q2QRILE0k",
+  authDomain: "dompalladar-8f4e2.firebaseapp.com",
+  projectId: "dompalladar-8f4e2",
+  storageBucket: "dompalladar-8f4e2.firebasestorage.app",
+  messagingSenderId: "990851986325",
+  appId: "1:990851986325:web:fe16529c93d90883dcbc9c",
+  measurementId: "G-HTMK8XYVGL"
+};
