67b33fcc4d4356a4140b3845283e81e9c08cb961
Vitor Queiroz <vitoralencar881@gmail.com>
2026-05-24 01:18:01 -0300

    implementa funcionalidade de 'reservas' já integrada ao Firestore

--- frontend/src/app/pages/reservas/reservas.ts
+++ frontend/src/app/pages/reservas/reservas.ts
@@ -1,9 +1,44 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-reservas',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './reservas.html',
   styleUrl: './reservas.css',
 })
-export class Reservas {}
+
+export class Reservas {
+  nome= '';
+  telefone= '';
+  pessoas= '';
+  data= '';
+  horario= '';
+  mesa= '';
+
+  constructor(private firestore: Firestore){}
+
+    async salvarReserva() {
+      try{
+        
+        await addDoc(
+          collection(this.firestore, 'reservas'),
+          {
+            nome: this.nome,
+            telefone: this.telefone,
+            pessoas: this.pessoas,
+            data: this.data,
+            horario: this.horario,
+            mesa: this.mesa
+          }
+        );
+
+        alert('Reserva realizada com Sucesso!');
+      
+      } catch (error){
+        console.error(error);
+      }
+    }
+}
