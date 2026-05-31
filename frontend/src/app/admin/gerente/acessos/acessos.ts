7f51e0c8164c3edd5793df276cdbee864aac99b2
Vitor Queiroz <vitoralencar881@gmail.com>
2026-05-31 20:11:49 -0300

    adiciona cadastro de funcionários no componente 'funcionários'
    Já integrado ao FireStore

--- frontend/src/app/admin/gerente/acessos/acessos.ts
+++ frontend/src/app/admin/gerente/acessos/acessos.ts
@@ -1,9 +1,48 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-acessos',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './acessos.html',
   styleUrl: './acessos.css',
 })
-export class Acessos {}
+export class Acessos {
+
+  funcionarios = [
+    {
+      nome: 'Kelvin Moises',
+      email: 'kelvin@gmail.com',
+      status: 'Ativo',
+
+      estoque: true,
+      pedidos: true,
+      cliente: false,
+      garcom: false,
+      esg: false
+    },
+    {
+      nome: 'Washington Lopes',
+      email: 'washwash@gmail.com',
+      status: 'Ativo',
+
+      estoque: false,
+      pedidos: true,
+      cliente: false,
+      garcom: false,
+      esg: false
+    },
+    {
+      nome: 'Thiago Silva',
+      email: 'thiago@gmail.com',
+      status: 'Pendente',
+
+      estoque: true,
+      pedidos: false,
+      cliente: false,
+      garcom: false,
+      esg: false
+    }
+  ];
+}
--- frontend/src/app/admin/gerente/funcionarios/funcionarios.ts
+++ frontend/src/app/admin/gerente/funcionarios/funcionarios.ts
@@ -1,9 +1,63 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Firestore, collection, addDoc, getDocs } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-funcionarios',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './funcionarios.html',
   styleUrl: './funcionarios.css',
 })
-export class Funcionarios {}
+
+export class Funcionarios {
+
+  nome= '';
+  email = '';
+
+  funcionarios: any[] = [];
+
+  constructor(private firestore: Firestore){
+  this.carregarFuncionarios();
+  }
+
+
+  
+  async carregarFuncionarios(){
+
+    const funcionarioRef = collection(this.firestore, 'funcionarios');
+
+    const snapshot = await getDocs(funcionarioRef);
+
+    this.funcionarios = snapshot.docs.map((item: any) => {
+
+      const dados = item.data();
+
+      return{
+        id: item.id, nome: dados.nome, email: dados.email
+        }
+        })
+
+  }
+
+      async salvarFuncionario(){
+
+        if(!this.nome || !this.email){
+
+          alert('Preencha todos os campos');
+          return;
+        }
+      
+        const funcionarioRef = collection(this.firestore, 'funcionarios');
+
+        await addDoc(funcionarioRef,{nome: this.nome, email: this.email});
+
+        await this.carregarFuncionarios();
+        this.nome = '';
+        this.email = '';
+
+            alert('Funcionário cadastrado com sucesso!')
+      }
+
+  }
