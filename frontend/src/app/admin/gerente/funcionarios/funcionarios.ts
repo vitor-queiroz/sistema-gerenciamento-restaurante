ef93d160b5bdbe92ed32ffead4f24986d6005256
Vitor Queiroz <vitoralencar881@gmail.com>
2026-05-31 21:04:38 -0300

    implementa CRUD de 'funcionários' integrado ao Firestore

--- frontend/src/app/admin/gerente/funcionarios/funcionarios.ts
+++ frontend/src/app/admin/gerente/funcionarios/funcionarios.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Firestore, collection, addDoc, getDocs } from '@angular/fire/firestore';
+import { Firestore, collection, addDoc, getDocs, deleteDoc, doc, updateDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-funcionarios',
@@ -16,14 +16,15 @@ export class Funcionarios {
   nome= '';
   email = '';
 
+  idFuncionarioEditando='';
+
   funcionarios: any[] = [];
 
-  constructor(private firestore: Firestore){
+  constructor(private firestore: Firestore, private cdr: ChangeDetectorRef){
   this.carregarFuncionarios();
+  
   }
 
-
-  
   async carregarFuncionarios(){
 
     const funcionarioRef = collection(this.firestore, 'funcionarios');
@@ -39,6 +40,7 @@ export class Funcionarios {
         }
         })
 
+        this.cdr.detectChanges();
   }
 
       async salvarFuncionario(){
@@ -48,16 +50,66 @@ export class Funcionarios {
           alert('Preencha todos os campos');
           return;
         }
-      
+
+
+
+        if(this.idFuncionarioEditando){
+
+          const funcionarioDoc = doc(this.firestore, 'funcionarios', this.idFuncionarioEditando);
+
+          await updateDoc(funcionarioDoc,{nome: this.nome, email: this.email});
+
+
+          this.idFuncionarioEditando = '';
+
+          await this.carregarFuncionarios();
+          this.cdr.detectChanges();
+
+          this.nome= '';
+          this.email= '';
+
+                alert('Funcionário atualizado com sucesso!');
+                return;
+          }
+
+
+          // AQUI VAI SERR PARA CADASTRAR O FUNCIONARIO
         const funcionarioRef = collection(this.firestore, 'funcionarios');
 
         await addDoc(funcionarioRef,{nome: this.nome, email: this.email});
 
         await this.carregarFuncionarios();
+        this.cdr.detectChanges();  
+
         this.nome = '';
         this.email = '';
 
             alert('Funcionário cadastrado com sucesso!')
+      }
+
+      editarFuncionario(funcionario: any) {
+
+      this.nome = funcionario.nome;
+      this.email = funcionario.email;
+
+      this.idFuncionarioEditando = funcionario.id;
+      }
+
+      async excluirFuncionario(id: string){
+
+        const confirmar = confirm('Deseja realmente excluir este funcionário?');
+
+        if(!confirmar){
+          return;
+        }
+
+        const funcionarioDoc = doc(this.firestore, 'funcionarios', id);
+
+        await deleteDoc(funcionarioDoc);
+        await this.carregarFuncionarios();
+        this.cdr.detectChanges();
+
+              alert('Funcionário excluído com sucesso!');
       }
 
   }
