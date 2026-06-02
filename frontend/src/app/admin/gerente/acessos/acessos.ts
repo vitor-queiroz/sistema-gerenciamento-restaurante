f89b652f3de2d1c5e329e78b505ddd8a3eb075e2
Vitor Queiroz <vitoralencar881@gmail.com>
2026-06-01 22:59:12 -0300

    integra 'funcionarios' ao controle de 'acessos' da gerência
    Essa integração foi simplesmente a coisa mais complexa que eu já fiz em todos os meu projetos, estava quase 3 dias para conseguir. Mas deu certo!!

--- frontend/src/app/admin/gerente/acessos/acessos.ts
+++ frontend/src/app/admin/gerente/acessos/acessos.ts
@@ -1,16 +1,42 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Firestore, collection, doc, updateDoc } from '@angular/fire/firestore';
+import { getDocs } from '@angular/fire/firestore';
+
 
 @Component({
   selector: 'app-acessos',
+  standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './acessos.html',
   styleUrl: './acessos.css',
 })
 export class Acessos {
 
+  funcionarios: any[] = [];
+
   funcionarioAberto = '';
+
+  constructor(private firestore: Firestore){
+    this.carregarFuncionarios();
+  }
+
+    
+  async carregarFuncionarios(){
+
+    const ref = collection(this.firestore, 'funcionarios');
+
+    const snapshot = await getDocs(ref);
+
+    console.log('Qnt documentos: ', snapshot.size)
+
+    this.funcionarios = snapshot.docs.map(item => ({id: item.id, ...item.data()
+  }));
+
+      console.log('Length:', this.funcionarios.length);
+      console.log(this.funcionarios);
+    }
 
   abrirPermissoes(id: string){
 
@@ -23,59 +49,35 @@ export class Acessos {
   }
 
 
-    aprovarAcesso(id: string){
-      const funcionario = this.funcionarios.find(f => f.id === id);
 
-      if(!funcionario) return;
+    async aprovarAcesso(id: string){
+      const ref = doc(this.firestore, 'funcionarios', id);
 
-      funcionario.status = 'Ativo'
+      await updateDoc(ref, {status: 'Ativo'});
+      
+            alert('Acesso aprovado com sucesso!');
     }
 
 
-    voltarParaPendente(id: string) {
-      const funcionario = this.funcionarios.find(f => f.id === id);
+    async voltarParaPendente(id: string) {
+      const ref = doc(this.firestore, 'funcionarios', id);
 
-      if (!funcionario) return;
-
-      funcionario.status = 'Pendente';
+      await updateDoc(ref, {status: 'Pendente'});
     }
 
-  funcionarios = [
-    {
-      id: '1',
-      nome: 'Kelvin Moises',
-      email: 'kelvin@gmail.com',
-      status: 'Ativo',
+    async salvarPermissoes(funcionario: any) {
 
-      estoque: true,
-      pedidos: true,
-      cliente: false,
-      garcom: false,
-      esg: false
-    },
-    {
-      id: '2',
-      nome: 'Washington Lopes',
-      email: 'washwash@gmail.com',
-      status: 'Ativo',
+      const ref = doc(this.firestore, 'funcionarios', funcionario.id);
 
-      estoque: false,
-      pedidos: true,
-      cliente: false,
-      garcom: false,
-      esg: false
-    },
-    {
-      id: '3',
-      nome: 'Thiago Silva',
-      email: 'thiago@gmail.com',
-      status: 'Pendente',
-
-      estoque: true,
-      pedidos: false,
-      cliente: false,
-      garcom: false,
-      esg: false
-    }
-  ];
+      await updateDoc(ref, {
+        cliente: funcionario.cliente,
+        pedidos: funcionario.pedidos, 
+        garcom: funcionario.garcom,
+        estoque: funcionario.estoque,
+        esg: funcionario.esg
+  });
+        alert('Permissões salvas com sucesso!')
 }
+
+}
+    
