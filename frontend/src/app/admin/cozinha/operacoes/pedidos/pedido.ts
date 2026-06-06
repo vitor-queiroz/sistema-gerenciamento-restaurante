3ab3934e7c1a8a0132e568d0d08c109685b5e877
Vitor Queiroz <vitoralencar881@gmail.com>
2026-06-05 22:29:15 -0300

    adiciona operação da cozinha no componente 'pedidos'
    Aqui, por enquanto, todos os pedidos vem do cardápio diretamente para o "fluxo da cozinha"

--- frontend/src/app/admin/cozinha/operacoes/pedidos/pedido.ts
+++ frontend/src/app/admin/cozinha/operacoes/pedidos/pedido.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Firestore, collection, getDocs, doc, updateDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-pedido',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './pedido.html',
   styleUrl: './pedido.css',
 })
-export class Pedido {}
+export class Pedido {
+  pedidos: any[] = [];
+
+  constructor(private firestore: Firestore,private cdr: ChangeDetectorRef){
+    this.carregarPedidos();
+  }
+
+  
+  //VEM UM POUR AQUI 
+  async carregarPedidos() {
+    const pedidosRef = collection(this.firestore, 'pedidos');
+
+    const snapshot = await getDocs(pedidosRef);
+
+    this.pedidos = snapshot.docs.map(item => ({
+      id: item.id,
+      ...item.data()
+    }));
+
+    this.cdr.detectChanges();
+  }
+
+  
+  //AQUI JA É OUTRO 
+  async atualizarStatus(id: string, status: string) {
+    const pedidoDoc = doc(this.firestore, 'pedidos', id);
+
+    await updateDoc(pedidoDoc, {
+      status: status
+    });
+
+    await this.carregarPedidos();
+
+    alert('Status do pedido atualizado!');
+  }
+}
