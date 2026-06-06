379af88d4b05c735e0d72a603cb8653dae0c065f
Vitor Queiroz <vitoralencar881@gmail.com>
2026-06-06 00:57:02 -0300

    adiciona controle de tempo dos 'pedido's

--- frontend/src/app/admin/cozinha/operacoes/pedidos/pedido.ts
+++ frontend/src/app/admin/cozinha/operacoes/pedidos/pedido.ts
@@ -36,13 +36,17 @@ export class Pedido {
   async atualizarStatus(id: string, status: string) {
     const pedidoDoc = doc(this.firestore, 'pedidos', id);
 
-    await updateDoc(pedidoDoc, {
-      status: status
-    });
-
+    const dadosAtualizados: any ={status: status};
+    
+    if(status === 'Finalizado'){
+      dadosAtualizados.finalizadoEm = new Date();
+    }
+    
+    await updateDoc(pedidoDoc, dadosAtualizados); 
     await this.carregarPedidos();
+    this.cdr.detectChanges();
 
-    alert('Status do pedido atualizado!');
+            console.log('Status atualizado!');
   }
 
 
@@ -70,5 +74,46 @@ export class Pedido {
   });
 
   return agrupados.filter(item => item.quantidade >= 3);
+}
+
+    mostrarHorarioPedido(criadoEm: any) {
+      if (!criadoEm) return '';
+
+      const data = criadoEm.toDate();
+
+  return data.toLocaleTimeString('pt-BR', {
+      hour: '2-digit',
+      minute: '2-digit'
+  });
+}
+
+    calcularMinutosPedido(pedido: any) {
+      if (!pedido.criadoEm) return '';
+
+      const inicio = pedido.criadoEm.toDate();
+
+      let fim = new Date();
+        if(pedido.status === 'Finalizado' && pedido.finalizadoEm){
+
+          fim= pedido.finalizadoEm.toDate();
+        }
+      
+      
+        const diferenca = fim.getTime() - inicio.getTime();
+            return Math.floor(diferenca / 60000);
+
+      }
+
+
+    verificarUrgenciaPedido(pedido: any) {
+      const minutos = this.calcularMinutosPedido(pedido);
+
+       if (minutos === '') return '';
+
+       if (minutos < 10) return 'pedido-recente';
+
+       if (minutos <= 15) return 'pedido-atencao';
+
+    return 'pedido-atrasado';
 }
 }
