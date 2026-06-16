8452b5653374a33e2a2916d1016a137756ad4bf2
Vitor Queiroz <vitoralencar881@gmail.com>
2026-06-15 22:16:42 -0300

    atualiza css 'home'
    + pequena alteração para fazer o deploy

---
+++ frontend/src/app/pages/cardapiopublic/cardapiopublic.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cardapiopublic',
+  imports: [],
+  templateUrl: './cardapiopublic.html',
+  styleUrl: './cardapiopublic.css',
+})
+export class Cardapiopublic {}
--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home{}
